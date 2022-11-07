2156398374f7c4a91d01f4a7f67f44deabc60aa6
IAMDESMOND <emmades247@gmail.com>
2022-11-07 21:00:49 +0100

    finished day2

--- day 2/app.js
+++ day 2/app.js
@@ -73,11 +73,14 @@ console.log(challenge.startsWith('30'));
 console.log(challenge.endsWith('JavaScript'));
 
 //no.23
+console.log(challenge.match(/a/g));
+
+//no.24
 let firstHalf = '30days of';
 
 console.log(firstHalf.concat(' JavaScript'))
 
-//no.24
+//no.25
 console.log(challenge.repeat(2))
 
 
@@ -113,16 +116,55 @@ let testSub = 'i hope this course is not full of jargon';
 console.log(testSub.includes('jargon'))
 
 //no.7
-console.log(Math.ceil(Math.random() * 100))
+console.log(Math.floor(Math.random() * 101))
 
 //no.8
-// i don't know
+console.log(Math.floor(Math.random() * 51) +50)
 
 
 //no.9
-console.log(Math.ceil(Math.random() * 255))
+console.log(Math.floor(Math.random() * 256))
 
 //no.10
 let stringData = 'javaScript'
 
-console.log(stringData.charAt(Math.ceil(Math.random() * stringData.length)));
+console.log(stringData.charAt(Math.floor(Math.random() * (stringData.length + 1))));    
+
+//no.11
+console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125');
+
+//no.12
+let start = conjunction.indexOf('because');
+
+console.log(conjunction.substr(start, 24));
+
+//Day 2 Exercise:Level 3
+
+//no.1
+let string = "'Love is the best thing in  the world. Some found their love and some are still looking for their love.'"
+
+let love = string.match(/love/gi);
+
+console.log(love.length);
+
+//no.2
+console.log(conjunction.match(/because/gi).length);
+
+//no.3
+ const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+ 
+
+ 
+ console.log(sentence.replace(/[%$#@&;!?]/gi, ''));
+
+ //no.4
+ let text = 'He earn 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+let income = text.match(/\d+/g);
+let salary = Number(income[0] * 12),
+    bonus = Number(income[1]),
+    onlineCourse = Number(income[2] * 12);
+
+let totalAnnualIncome = salary + bonus + onlineCourse
+
+ console.log(totalAnnualIncome);
