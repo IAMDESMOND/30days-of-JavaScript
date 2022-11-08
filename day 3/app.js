7196a08988d82dd0f004b0d1a7378516bf159041
IAMDESMOND <emmades247@gmail.com>
2022-11-08 21:57:16 +0100

    day 3 started

--- day 3/app.js
+++ day 3/app.js
@@ -1,91 +1,73 @@
-// let word = 'javascript';
+//Day 3 Exercises: level 1
 
-// word[0] = 'Y'
+//no.1
+let firstName = 'Desmond';
+let lastName = 'Emmanuel';
+let country = 'Nigeria';
+let city = 'Lagos';
+let age = 16;
+let isMarried = 'single';
+let year = new Date().getFullYear;
+
+console.log(typeof firstName,
+            typeof lastName,
+            typeof country,
+            typeof city,
+            typeof age,
+            typeof isMarried,
+            typeof year
+    );
+
+//no.2
+console.log(typeof '10');
+
+//no.3
+console.log(parseInt(9.8));
+
+//no.4
+console.log(true);
+console.log(1 === 1);
+console.log(4 > 3);
+console.log(false);
+console.log(2 === 1);
+console.log(4 < 3);
+
+//no.5
+console.log(4 > 3);  //true
+console.log(4 >= 3);  //true 
+console.log(4 < 3);  //false
+console.log(4 <= 3);  //false
+console.log(4 == 4);  //true
+console.log( 4 === 4); //true
+console.log(4 != 4);  //false
+console.log(4 !== 4);  //false
+console.log( 4 != '4'); //false
+console.log(4 == '4'); //true
+console.log(4 === '4') ; // false
+console.log( ('jargon'.length) == ('python'.length));
+
+//no.6
+
+console.log(4 > 3 && 10 < 12);  // true
+console.log(4 > 3 && 10 > 12); // false
+console.log( 4 > 3 || 10 < 12);// true
+console.log( 4 > 3 || 10 > 12);// true
+console.log(!(4 > 3)); // false
+console.log(!(4 < 3)); // true
+console.log(!(false)); // true
+console.log(!(4 > 3 && 10 < 12)); // false
+console.log(!(4 > 3 && 10 > 12)); // true
+console.log( !(4 === '4'));// true
+console.log(!(('python'.includes('on')) && ('jargon'.includes('on'))))// false
 
 
-// console.log(word);
+//no.7
+let datObj = new Date();
 
-// let numOne = 3
-// let numTwo = 3
-
-// console.log(numOne == numTwo)
-
-// let js = 'JavaScript'
-// let py = 'JavaScript'
-
-// console.log(js == py)   
-
-// let lightOn = false
-// let lightOff = false
-
-// console.log(lightOn == lightOff)
-
-// // let nums = [1, 2, 3]
-// // let numbers = [1, 2, 3]
-
-
-// console.log(nums == numbers);
-
-// let userOne = {
-//     name:'Asabeneh',
-//     role:'teaching',
-//     country:'Finland'
-//     }
-    
-// let userTwo = {
-//     name:'Asabeneh',
-//     role:'teaching',
-//     country:'Finland'
-//     }
-
-//     console.log(userOne == userTwo)
-
-// let nums = [1, 2, 3]
-// let numbers =  nums
-
-// console.log(nums == numbers)
-
-// let age = 35
-// const gravity = 9.81  // we use const for non-changing values, gravitational constant in  m/s2
-// let mass = 72         // mass in Kilogram
-// const PI = 3.14       // pi a geometrical constant
-
-// // More Examples
-// const boilingPoint = 100 // temperature in oC, boiling point of water which is a constant
-// const bodyTemp = 37      // oC average human body temperature, which is a constant
-
-// console.log(age, gravity, mass, PI, boilingPoint, bodyTemp)
-
-// const PI = Math.PI
-
-// console.log(PI)
-
-// console.log(Math.round(PI))
-// console.log(Math.round(9.81))  
-// console.log(Math.floor(PI))
-// console.log(Math.floor(9.81))   
-// console.log(Math.ceil(PI))
-
-// const randNum = Math.random() 
-// console.log(randNum)
-
-// const num = Math.ceil(Math.random () * 0)// number between 0 and 20
-
-// console.log(num)
-
-// console.log(Math.sqrt(25))
-
-// console.log('I hope everyone is enjoying\t the 30 Days Of JavaScript challenge.\nDo you ?') 
-
-// //Syntax
-// `String literal text ${expression}`
-
-let string = 'JavaScript'
-
-console.log(string.substring(7))       // Script
-
-// let country = 'Finland'
-
-// console.log(country.substring(0, 3))   // Fin
-// console.log(country.substring(3, 7))   // land
-// console.log(country.substring(3))      // land
+console.log(datObj.getFullYear())
+console.log(datObj.getMonth() + 1);
+console.log(datObj.getDate());
+console.log(datObj.getDay());
+console.log(datObj.getHours());
+console.log(datObj.getMinutes());
+console.log(datObj.getTime());
