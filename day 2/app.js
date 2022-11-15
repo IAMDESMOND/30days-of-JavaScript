3a8c6a63d2a9a5fce63d448ab02d00d71e401caf
IAMDESMOND <emmades247@gmail.com>
2022-11-15 22:06:05 +0100

    finished days 3

--- day 2/app.js
+++ day 2/app.js
@@ -1,170 +1,170 @@
-//day 2 Exercises: level 1
+// //day 2 Exercises: level 1
 
-//no.1
-let challenge = '30 Days Of JavaScript';
+// //no.1
+// let challenge = '30 Days Of JavaScript';
 
-//no.2
-console.log(challenge);
+// //no.2
+// console.log(challenge);
 
-//no.3
-console.log(challenge.length);
+// //no.3
+// console.log(challenge.length);
 
-//no.4
-console.log(challenge.toUpperCase());
+// //no.4
+// console.log(challenge.toUpperCase());
 
-//no.5
-console.log(challenge.toLowerCase());
+// //no.5
+// console.log(challenge.toLowerCase());
 
-//no.6
-console.log(challenge.substring(0, 2));
+// //no.6
+// console.log(challenge.substring(0, 2));
 
-//no.7
-console.log(challenge.substring(2, 21));
+// //no.7
+// console.log(challenge.substring(2, 21));
 
-//no.8
-console.log(challenge.includes('Script'));
+// //no.8
+// console.log(challenge.includes('Script'));
 
-//no.9
-console.log(challenge.split(''));
+// //no.9
+// console.log(challenge.split(''));
 
-//no.10
-console.log(challenge.split(' '));
+// //no.10
+// console.log(challenge.split(' '));
 
-//no.11
-let brands = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+// //no.11
+// let brands = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
 
-console.log(brands.split(','))
+// console.log(brands.split(','))
 
-//no.12
-console.log(challenge.replace('30 Days Of JavaScript', '30 Days Of Python'));
+// //no.12
+// console.log(challenge.replace('30 Days Of JavaScript', '30 Days Of Python'));
 
-//no.13
-console.log(challenge.charAt(15))
+// //no.13
+// console.log(challenge.charAt(15))
 
-//no.14
-console.log(challenge.charCodeAt(challenge.indexOf('J')))
+// //no.14
+// console.log(challenge.charCodeAt(challenge.indexOf('J')))
 
-//no.15
-console.log(challenge.indexOf('a'))
+// //no.15
+// console.log(challenge.indexOf('a'))
 
-//no.16
-console.log(challenge.lastIndexOf('a'));
+// //no.16
+// console.log(challenge.lastIndexOf('a'));
 
-//no.17
-let conjunction = 'you cannot end a sentence with because because because is a conjunction';
+// //no.17
+// let conjunction = 'you cannot end a sentence with because because because is a conjunction';
 
-console.log(conjunction.indexOf('because'));
+// console.log(conjunction.indexOf('because'));
 
-//no.18
-console.log(conjunction.lastIndexOf('because'));
+// //no.18
+// console.log(conjunction.lastIndexOf('because'));
 
-//no.19
-console.log(conjunction.search('because'));
+// //no.19
+// console.log(conjunction.search('because'));
 
-//no.20
-let trimSubject = '  hello world  '
+// //no.20
+// let trimSubject = '  hello world  '
 
-console.log(trimSubject.trim());
+// console.log(trimSubject.trim());
 
-//no.21
-console.log(challenge.startsWith('30'));
+// //no.21
+// console.log(challenge.startsWith('30'));
 
-//no.22
-console.log(challenge.endsWith('JavaScript'));
+// //no.22
+// console.log(challenge.endsWith('JavaScript'));
 
-//no.23
-console.log(challenge.match(/a/g));
+// //no.23
+// console.log(challenge.match(/a/g));
 
-//no.24
-let firstHalf = '30days of';
+// //no.24
+// let firstHalf = '30days of';
 
-console.log(firstHalf.concat(' JavaScript'))
+// console.log(firstHalf.concat(' JavaScript'))
 
-//no.25
-console.log(challenge.repeat(2))
-
-
-
-//day 2 Exercise:level 2
-
-//no.1
-console.log("The quote 'there is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another");
-
-//no.2
-console.log(`"love is not about patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so dont't just give money but reach out your hand instead."`)
-
-//no.3
-console.log(typeof '10');
-
-console.log(typeof parseInt('10'));
-
-//no.4
-console.log(parseFloat('9.8'))
-
-console.log(Math.round(parseFloat('9.8')));
-
-//no.5
-let python = 'python',
-    jargon = 'jargon';
-
-console.log(python.includes('on'))
-console.log(jargon.includes('on'));
-
-//no.6
-let testSub = 'i hope this course is not full of jargon';
-
-console.log(testSub.includes('jargon'))
-
-//no.7
-console.log(Math.floor(Math.random() * 101))
-
-//no.8
-console.log(Math.floor(Math.random() * 51) +50)
+// //no.25
+// console.log(challenge.repeat(2))
 
 
-//no.9
-console.log(Math.floor(Math.random() * 256))
 
-//no.10
-let stringData = 'javaScript'
+// //day 2 Exercise:level 2
 
-console.log(stringData.charAt(Math.floor(Math.random() * (stringData.length + 1))));    
+// //no.1
+// console.log("The quote 'there is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another");
 
-//no.11
-console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125');
+// //no.2
+// console.log(`"love is not about patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so dont't just give money but reach out your hand instead."`)
 
-//no.12
-let start = conjunction.indexOf('because');
+// //no.3
+// console.log(typeof '10');
 
-console.log(conjunction.substr(start, 24));
+// console.log(typeof parseInt('10'));
 
-//Day 2 Exercise:Level 3
+// //no.4
+// console.log(parseFloat('9.8'))
 
-//no.1
-let string = "'Love is the best thing in  the world. Some found their love and some are still looking for their love.'"
+// console.log(Math.round(parseFloat('9.8')));
 
-let love = string.match(/love/gi);
+// //no.5
+// let python = 'python',
+//     jargon = 'jargon';
 
-console.log(love.length);
+// console.log(python.includes('on'))
+// console.log(jargon.includes('on'));
 
-//no.2
-console.log(conjunction.match(/because/gi).length);
+// //no.6
+// let testSub = 'i hope this course is not full of jargon';
 
-//no.3
- const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+// console.log(testSub.includes('jargon'))
+
+// //no.7
+// console.log(Math.floor(Math.random() * 101))
+
+// //no.8
+// console.log(Math.floor(Math.random() * 51) +50)
+
+
+// //no.9
+// console.log(Math.floor(Math.random() * 256))
+
+// //no.10
+// let stringData = 'javaScript'
+
+// console.log(stringData.charAt(Math.floor(Math.random() * (stringData.length + 1))));    
+
+// //no.11
+// console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125');
+
+// //no.12
+// let start = conjunction.indexOf('because');
+
+// console.log(conjunction.substr(start, 24));
+
+// //Day 2 Exercise:Level 3
+
+// //no.1
+// let string = "'Love is the best thing in  the world. Some found their love and some are still looking for their love.'"
+
+// let love = string.match(/love/gi);
+
+// console.log(love.length);
+
+// //no.2
+// console.log(conjunction.match(/because/gi).length);
+
+// //no.3
+//  const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
  
 
  
- console.log(sentence.replace(/[%$#@&;!?]/gi, ''));
+//  console.log(sentence.replace(/[%$#@&;!?]/gi, ''));
 
- //no.4
- let text = 'He earn 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+//  //no.4
+//  let text = 'He earn 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 
-let income = text.match(/\d+/g);
-let salary = Number(income[0] * 12),
-    bonus = Number(income[1]),
-    onlineCourse = Number(income[2] * 12);
+// let income = text.match(/\d+/g);
+// let salary = Number(income[0] * 12),
+//     bonus = Number(income[1]),
+//     onlineCourse = Number(income[2] * 12);
 
-let totalAnnualIncome = salary + bonus + onlineCourse
+// let totalAnnualIncome = salary + bonus + onlineCourse
 
- console.log(totalAnnualIncome);
+//  console.log(totalAnnualIncome);
--- day 3/app.js
+++ day 3/app.js
@@ -1,73 +1,223 @@
 //Day 3 Exercises: level 1
 
 //no.1
-let firstName = 'Desmond';
-let lastName = 'Emmanuel';
-let country = 'Nigeria';
-let city = 'Lagos';
-let age = 16;
-let isMarried = 'single';
-let year = new Date().getFullYear;
+// let firstName = 'Desmond';
+// let lastName = 'Emmanuel';
+// let country = 'Nigeria';
+// let city = 'Lagos';
+// let age = 16;
+// let isMarried = 'single';
+// let year = new Date().getFullYear;
 
-console.log(typeof firstName,
-            typeof lastName,
-            typeof country,
-            typeof city,
-            typeof age,
-            typeof isMarried,
-            typeof year
-    );
+// console.log(typeof firstName,
+//             typeof lastName,
+//             typeof country,
+//             typeof city,
+//             typeof age,
+//             typeof isMarried,
+//             typeof year
+//     );
+
+// //no.2
+// console.log(typeof '10');
+
+// //no.3
+// console.log(parseInt(9.8));
+
+// //no.4
+// console.log(true);
+// console.log(1 === 1);
+// console.log(4 > 3);
+// console.log(false);
+// console.log(2 === 1);
+// console.log(4 < 3);
+
+// //no.5
+// console.log(4 > 3);  //true
+// console.log(4 >= 3);  //true 
+// console.log(4 < 3);  //false
+// console.log(4 <= 3);  //false
+// console.log(4 == 4);  //true
+// console.log( 4 === 4); //true
+// console.log(4 != 4);  //false
+// console.log(4 !== 4);  //false
+// console.log( 4 != '4'); //false
+// console.log(4 == '4'); //true
+// console.log(4 === '4') ; // false
+// console.log( ('jargon'.length) == ('python'.length));
+
+// //no.6
+
+// console.log(4 > 3 && 10 < 12);  // true
+// console.log(4 > 3 && 10 > 12); // false
+// console.log( 4 > 3 || 10 < 12);// true
+// console.log( 4 > 3 || 10 > 12);// true
+// console.log(!(4 > 3)); // false
+// console.log(!(4 < 3)); // true
+// console.log(!(false)); // true
+// console.log(!(4 > 3 && 10 < 12)); // false
+// console.log(!(4 > 3 && 10 > 12)); // true
+// console.log( !(4 === '4'));// true
+// console.log(!(('python'.includes('on')) && ('jargon'.includes('on'))))// false
+
+
+// //no.7
+// let datObj = new Date();
+
+// console.log(datObj.getFullYear())
+// console.log(datObj.getMonth() + 1);
+// console.log(datObj.getDate());
+// console.log(datObj.getDay());
+// console.log(datObj.getHours());
+// console.log(datObj.getMinutes());
+// console.log(datObj.getTime());
+
+//level 2 
+
+//no.1
+
+// let base = prompt('Enter base');
+// let height = prompt('Enter height');
+// let area = 0.5 * Number(base) * Number(height)
+
+// console.log(`Base: ${base}`);
+// console.log(`Height: ${height}`);
+// console.log(`The Area of the triangle is ${area}`)
 
 //no.2
-console.log(typeof '10');
+
+// let sideA = Number(prompt('Enter side A'));
+// let sideB = Number(prompt('Enter side B'));
+// let sideC = Number(prompt('Enter side C'))
+// let perimeter = sideA + sideB + sideC;
+
+// console.log(`side A: ${sideA}`);
+// console.log(`side B: ${sideB}`);
+// console.log(`side C: ${sideC}`);
+// console.log(`The perimeter of the triangle is ${perimeter}`)
 
 //no.3
-console.log(parseInt(9.8));
+
+// let length = Number(prompt('Enter length'));
+// let width = Number(prompt('Enter width'));
+// let area =  length * width
+// let perimeter = 2 * (length + width);
+// console.log(`length: ${length}`);
+// console.log(`width: ${width}`);
+// console.log(`The Area of the rectangle is ${area}`)
+// console.log(`The perimeter of the rectangle is ${perimeter}`)
 
 //no.4
-console.log(true);
-console.log(1 === 1);
-console.log(4 > 3);
-console.log(false);
-console.log(2 === 1);
-console.log(4 < 3);
+//  const pi = 3.14
+//  let radius = Number(prompt('Enter Radius'));
+//  let area =  pi * radius ** 2
+//  let circumference = 2 * pi * radius;
+//  console.log(`Radius: ${radius}`);
+//  console.log(`The Area of the cicle is ${area}`)
+//  console.log(`The circumference of the cicle is ${circumference}`)
 
 //no.5
-console.log(4 > 3);  //true
-console.log(4 >= 3);  //true 
-console.log(4 < 3);  //false
-console.log(4 <= 3);  //false
-console.log(4 == 4);  //true
-console.log( 4 === 4); //true
-console.log(4 != 4);  //false
-console.log(4 !== 4);  //false
-console.log( 4 != '4'); //false
-console.log(4 == '4'); //true
-console.log(4 === '4') ; // false
-console.log( ('jargon'.length) == ('python'.length));
+// let X1 = 0,
+//     X2 = 1,
+//     Y1 = 2 * X1 -2,
+//     Y2 = 2 * X2 -2,
+//     dY = Y2 - Y1,
+//     dX = X2 - X1,
+//     slope = dY/dX;
+
+// console.log(slope);    
 
 //no.6
+// let x1 = 2,
+//     y1 = 2,
+//     x2 = 6,
+//     y2 = 10,
+//     dy = y2 - y1,
+//     dx = x2 - x1,
+//     m = dy/dx;
 
-console.log(4 > 3 && 10 < 12);  // true
-console.log(4 > 3 && 10 > 12); // false
-console.log( 4 > 3 || 10 < 12);// true
-console.log( 4 > 3 || 10 > 12);// true
-console.log(!(4 > 3)); // false
-console.log(!(4 < 3)); // true
-console.log(!(false)); // true
-console.log(!(4 > 3 && 10 < 12)); // false
-console.log(!(4 > 3 && 10 > 12)); // true
-console.log( !(4 === '4'));// true
-console.log(!(('python'.includes('on')) && ('jargon'.includes('on'))))// false
+// console.log(m);    
+
+// //no.7
+// console.log(slope == m);
+
+//no.8
+// let X = -3;
+// let Y = (X ** 2) + (6 * X) + 9
+
+// Y == 0 ? console.log(`Y = 0 where X = ${X}`) : console.log(`Y = ${Y}`);
+
+// //no.9
+// let hours = prompt('Enter hours');
+// let ratePerHour = prompt('Enter rate Per Hour');
+// let weeklyEarning = Number(hours) * Number(ratePerHour)
+// console.log(`hours: ${hours}`);
+// console.log(`rate Per Hour: ${ratePerHour}`);
+// console.log(`Your weekly Earning is ${weeklyEarning}`)
+
+//n0.10
+// 'desmond'.length > 7 ? console.log( 'your name is long') : console.log('your name is short.');
+
+//no.11
+// let firstName = 'Emmanuel'
+// let lastName = 'Desmond'
+
+// firstName.length > lastName.length 
+// ? console.log( `Your first name, ${firstName} is longer than your family name, ${lastName}`) : console.log(`Your family name, ${lastName} is longer than your first name, ${firstName}`);
+
+//no.12
+// let myAge = 25;
+// let yourAge = 200;
+
+// myAge > yourAge 
+//    ? console.log(`I am ${myAge - yourAge} years older than you`)
+//    : console.log(`I am ${yourAge - myAge} years older than you`);
+
+//no.13
+// let yourBirthYear = Number(prompt('Enter your Birth year'));
+// let year = new Date().getFullYear();
+// let yourAge = year - yourBirthYear;
+
+// yourAge < 18
+//     ? console.log(`You are ${yourAge}. You will be allowed to drive after ${18 - yourAge} years`)
+//     : console.log(`You are ${yourAge}. You are old enough to drive`);
+
+//no.14
+// let year = new Date().getFullYear();
+// let yearsLived = Number(prompt('Enter number of years you live'));
+// let yourBirthYear = year - yearsLived
+// let secondsInYear = 60 * 60 * 24 * 365;
+// let secondsLived = yearsLived * secondsInYear;
+
+// console.log(`You are ${yearsLived}, You have lived ${secondsLived} seconds, Your year of Birth is ${yourBirthYear}.`);
+
+//n0.15
+// let time = new Date(),
+//     timeInYear = time.getFullYear(),
+//     timeInMonth = time.getMonth(),
+//     timeInDay = time.getDate(),
+//     timeInHour = time.getHours(),
+//     timeInMinutes = time.getMinutes();
+
+// console.log(`${timeInYear}-${timeInMonth}-${timeInDay} ${timeInHour}:${timeInMinutes
+// }`);    
+// console.log(`${timeInDay}-${timeInMonth}-${timeInYear} ${timeInHour}:${timeInMinutes
+// }`);    
+// console.log(`${timeInDay}/${timeInMonth}/${timeInYear} ${timeInHour}:${timeInMinutes
+// }`);    
 
 
-//no.7
-let datObj = new Date();
+//level 3
 
-console.log(datObj.getFullYear())
-console.log(datObj.getMonth() + 1);
-console.log(datObj.getDate());
-console.log(datObj.getDay());
-console.log(datObj.getHours());
-console.log(datObj.getMinutes());
-console.log(datObj.getTime());
+//no.1
+// let time = new Date(),
+//     timeInYear = time.getFullYear(),
+//     timeInMonth = time.getMonth(),
+//     timeInDay = time.getDate(),
+//     timeInHour = 1,
+//     timeInMinutes = 2;
+
+// let hours = String(timeInHour).length < 2 ;
+// let minutes = String(timeInMinutes).length < 2 ;
+
+// hours !== minutes ? console.log(`${timeInYear}-${timeInMonth}-${timeInDay} 0${timeInHour}:0${timeInMinutes}`): console.log(`${timeInYear}-${timeInMonth}-${timeInDay} ${timeInHour}:${timeInMinutes}`);
