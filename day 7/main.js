5f29f7194b1690902005e651cc87a16ee0cdbce2
IAMDESMOND <emmades247@gmail.com>
2022-12-24 14:18:43 +0100

    finish level 1

---
+++ day 7/main.js
@@ -0,0 +1,163 @@
+function printFullName() {
+    let firstName = 'Desmond',
+        lastName = ' Emmanuel'
+
+    console.log(firstName + lastName);    
+}
+
+printFullName()
+
+function fullName(firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
+console.log(fullName('Desmond', 'Emmanuel'));
+
+function addNumbers(num1, num2) {
+    sum = num1 + num2
+    return sum
+}
+
+console.log(addNumbers(10, 20))
+
+function areaOfRectangle(length, width) {
+    let area = length * width;
+    return `area of rectangle = ${area}`
+}
+console.log(areaOfRectangle(2, 2)); 
+
+function perimeterOfRectangle(length, width) {
+    let perimeter = 2 * (length + width);
+    return  `perimeter of rectangle = ${perimeter}`
+}
+console.log(perimeterOfRectangle(2, 2));
+
+function volumeOfRectPrism(length, width, height) {
+    let volume = length * width * height;
+    return volume
+}
+
+console.log(volumeOfRectPrism(2, 2, 2))
+
+function areaOfCircle(radius) {
+    let area = Math.PI * radius * radius
+    return area      
+}
+
+console.log(areaOfCircle(7));
+
+function circumOfCircle(radius) {
+    let circum = 2 * Math.PI * radius
+    return circum      
+}
+
+console.log(circumOfCircle(7));
+
+function densityOfSubstances(mass, volume) {
+    let density = mass / volume
+    return density 
+}
+
+console.log(densityOfSubstances(100, 50))
+
+function calcSpeed(distance, time) {
+    let speed = distance / time
+    return speed
+}
+console.log(calcSpeed(120, 20))
+
+function calcWeight(mass, gravity = 9.8) {
+    area = mass * gravity
+    return area      
+}
+console.log(calcWeight(10))
+
+function convertCelsiusToFahrenheit(celsius) {
+    let fahrenheit = (celsius * (9/5)) + 32
+    return fahrenheit
+}
+console.log(convertCelsiusToFahrenheit(100))
+
+function BMI(weight, height) {
+    let bmi = weight / (height * height)
+
+    switch (true) {
+        case bmi < 18.5:
+            return `${bmi} Underweight`
+            break;
+        case (bmi == 18.5) && (bmi <= 24.9):
+            return `${bmi} Normal weight`
+            break;
+        case (bmi == 25) && (bmi <= 29.9):
+            return `${bmi} Overweight`
+            break;
+        case (bmi >= 30):
+            return `${bmi} Overweight`
+            break;
+        default:
+            break;
+    }
+}
+
+console.log(BMI(6000, 10))
+
+function checkSeason(month) {
+    switch (month) {
+    case 'january':
+        return ('Season is winter')
+        break;
+    case 'february':
+        return ('Season is winter')
+        break;
+    case 'march':
+        return ('Season is spring')
+        break;
+    case 'april':
+        return ('Season is spring')
+        break;
+    case 'may':
+        return ('Season is spring')
+        break;
+    case 'june':
+        return ('Season is summer')
+        break;
+    case 'july':
+        return ('Season is summer')
+        break;                             
+    case 'august':
+        return ('Season is summer')
+        break;
+    case 'september':
+        return ('Season is autumn')
+        break; 
+    case 'october':
+        return ('Season is autumn')
+        break;
+    case 'november':
+        return ('Season is autumn')
+        break;
+    case 'december':
+        return ('Season is winter')
+        break;                
+    default:
+        return ('not a month')
+        break;
+}
+}
+
+console.log(checkSeason('may'))
+
+function findMax(arg1, arg2, arg3) {
+    const arr = [arg1, arg2, arg3]
+    let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (max < element) {
+            max = element
+        }
+    }
+    return max
+}
+
+console.log(findMax(0, 10, 5))
+
+console.log(findMax(0, -10, -2))
