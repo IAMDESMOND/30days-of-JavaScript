3aa81060870b7d9c5f9c3c2d696098ebb3c87a35
IAMDESMOND <emmades247@gmail.com>
2023-02-06 08:34:44 +0100

    done with day 15

--- day 15/main.js
+++ day 15/main.js
@@ -1,80 +1,118 @@
 class Animal {
-    constructor(name, age, color, legs) {
-        this.name = name
-        this.age = age
-        this.color = color
-        this.legs = legs
-    }
-    fullDetails() {
-        let info = `
-            Name: ${this.name}\n
-            Age: ${this.age}\n
-            Color: ${this.color}\n
-            Legs: ${this.legs}
-            `
-    return info
-    }
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  fullInfo() {
+    return `my pets name is ${this.name} and it's ${this.age} yrs old`
+  }
 }
-class Person {
-            constructor(firstName, lastName, age, country, city) {
-              this.firstName = firstName
-              this.lastName = lastName
-              this.age = age
-              this.country = country
-              this.city = city
-              this.score = 0
-              this.skills = []
-            }
-            getFullName() {
-              const fullName = this.firstName + ' ' + this.lastName
-              return fullName
-            }
-            get getScore() {
-              return this.score
-            }
-            get getSkills() {
-              return this.skills
-            }
-            set setScore(score) {
-              this.score += score
-            }
-            set setSkill(skill) {
-              this.skills.push(skill)
-            }
-            getPersonInfo() {
-              let fullName = this.getFullName()
-              let skills =
-                this.skills.length > 0 &&
-                this.skills.slice(0, this.skills.length - 1).join(', ') +
-                  ` and ${this.skills[this.skills.length - 1]}`
-              let formattedSkills = skills ? `He knows ${skills}` : ''
-          
-              let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`
-              return info
-            }
-          }
-          
-          const person1 = new Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
-          const person2 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
-          const person3 = new Person('John', 'Doe', 50, 'Mars', 'Mars city')
-          
-          person1.setScore = 1
-          person1.setSkill = 'HTML'
-          person1.setSkill = 'CSS'
-          person1.setSkill = 'JavaScript'
-          
-          person2.setScore = 1
-          person2.setSkill = 'Planning'
-          person2.setSkill = 'Managing'
-          person2.setSkill = 'Organizing'
-          
-          console.log(person1.getScore)
-          console.log(person2.getScore)
-          
-          console.log(person1.getSkills)
-          console.log(person2.getSkills)
-          console.log(person3.getSkills)
-          
-          console.log(person1.getPersonInfo())
-          console.log(person2.getPersonInfo())
-          console.log(person3.getPersonInfo())
+
+class Dog extends Animal {
+  saySomething() {
+    return 'I am a dog.'
+  }
+  fullInfo(){
+    return `my Dogs name is ${this.name} and it's ${this.age} yrs old`
+  }
+  
+}
+
+
+class Cat extends Animal {
+  saySomething() {
+    return 'i am a cat'
+  }
+}
+
+let cap = new Dog('cap', 2)
+
+
+console.log(cap.fullInfo());
+console.log(cap.saySomething());
+
+
+class Statistics {
+  constructor(arr) {
+    this.nums = arr
+  }
+  order(){
+    let order = this.nums.sort((x, y) => x - y)
+    return order
+  }
+  count(){
+    let count = 0
+     this.nums.forEach(a => {
+      return count++
+    });
+    return count
+  }
+  sum(){
+    let sumOfNums = this.order().reduce((acc, curr) => acc + curr)
+    return sumOfNums
+  }
+  min(){
+    return this.order()[0]
+  }
+  max(){
+    return this.order()[this.nums.length -1]
+  }
+  range(){
+    return this.max() - this.min()
+  }
+  mean(){
+    let mean = this.sum() / this.nums.length
+    return Math.round(mean)
+  }
+  median(){
+    let median
+    if (this.nums.length % 2 == 0) {
+      median = (this.order()[this.count()/2] + this.order()[this.count()/2 + 1])/ 2
+    } else {
+      median = this.order()[Math.round(this.count()/2)]
+    }
+    return median
+  }
+  mode(){
+    let objs= {}
+    let max = 0
+    let count = 0
+    this.order().forEach(function(e) {
+      let num = e
+      if(!objs[e]) {
+        objs[e] = 1
+      }else{objs[e]++}
+      
+      if (count < objs[e]) {
+        max = e
+        count = objs[e]
+      }
+    })
+  return max
+  }
+  variance(){
+    let sumDiff = 0
+    this.order().forEach(num => {
+      sumDiff += (num - this.mean()) ** 2
+    })
+    return sumDiff/ this.count()
+  }
+  std(){
+    return +Math.sqrt(this.variance()).toFixed(2)
+  } 
+}
+
+const ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+let statistics = new Statistics(ages)
+
+
+console.log(statistics.order());
+console.log(statistics.count());
+console.log(statistics.min());
+console.log(statistics.max());
+console.log(statistics.range());
+console.log(statistics.mean());
+console.log(statistics.median());
+console.log(statistics.mode());
+console.log(statistics.variance())
+console.log(statistics.std())
